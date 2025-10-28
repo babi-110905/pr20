311e44efc63354a1996d81fced2f21af4b35a0e9
babi-110905 <babi110905@gmail.com>
2025-10-28 06:59:31 +0530

    Add files via upload

---
+++ ShoppingBill.java
@@ -0,0 +1,7 @@
+public class ShoppingBill {
+    public static void main(String[] args) {
+        double item1 = 200.50, item2 = 300.75;
+        double total = item1 + item2;
+        System.out.println("Total Shopping Bill: " + total);
+    }
+}
